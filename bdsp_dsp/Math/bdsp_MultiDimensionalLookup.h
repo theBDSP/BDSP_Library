0a4540e9c386b50e42c898f15aac81849ebf3f2d
Ben Shafran <bdsp@thebdsp.com>
2025-01-29 10:35:46 -0500

    Squashed commit of the following:
    commit c63686e5c60025fd8ebc47d5960dbe9bbb55ac23
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Wed Jan 29 10:33:22 2025 -0500
    
        Documented Math Classes
    
        Added documentation to most of the math dsp classes.
    
        Fixed error in construction of Hadamard Matrix, also renamed the method to be more clear.
    
    commit f4ad187a7359c522716362b7313faad02abdd10b
    Author: theBDSP <70613790+theBDSP@users.noreply.github.com>
    Date:   Wed Jan 29 10:24:18 2025 -0500
    
        Silence Xcode Errors
    
        Mostly initialization order, unused functions, missing virtual destructors, missing overrides.
    
        One instance of returning a pointer to a temporary variable was fixed in the StateHandler class.
    
    commit 55688f1fa92efe3dd0a4371a266d78ade0e27271
    Merge: ca9d97a 25d7555
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Mon Jan 20 11:51:17 2025 -0500
    
        Merge branch 'Develop' of https://github.com/theBDSP/BDSP_Library into Develop
    
    commit 25d7555863710355d628771563e1e6360bb84ec1
    Author: theBDSP <70613790+theBDSP@users.noreply.github.com>
    Date:   Sun Jan 19 11:37:05 2025 -0500
    
        Update bdsp_RingModulation.h
    
    commit 0ff376236a9de321e950a1bec569f0cf53ef2060
    Merge: aa18487 a62f518
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Sun Jan 19 11:23:12 2025 -0500
    
        Merge in code not added to Develop
    
        Some code was mistakenly added to main without adding it to develop. Merging main into develop to add the missing code.
    
    commit ca9d97a61096bfab448eca24bf5c375e3ee4e745
    Merge: aa18487 a62f518
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Sun Jan 19 11:23:12 2025 -0500
    
        Merge in code not added to Develop
    
        Some code was mistakenly added to main without adding it to develop. Merging main into develop to add the missing code.
    
    commit aa184874bcea3238766f30185b6b4e32d0b055a6
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Sun Jan 19 11:05:26 2025 -0500
    
        Updated Ring Modulation
    
        Removed unused legacy code from the Ring Modulation Class.
        Remove unused MultiSignalProcessorBase class.
        Reduced branching processing code of Ring Modulation.
    
        Added option in BasicWaveGenerator to out bipolar/unipolar singal.
    
        Added documentation to RingModulation class.
    
    commit 5456a6c4ace2534d969f58b93c2780e10760f429
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Thu Jan 16 15:20:31 2025 -0500
    
        Generators Rework
    
        Split different generator classes into separate header files.
        Removed unused second argument (int smp) from getSample method of GeneratorBase.
        Added documentation to generator classes.
    
    commit b7ed498f2a00af159df28ef94434d991f0a1c3b3
    Author: Ben Shafran <bdsp@thebdsp.com>
    Date:   Thu Jan 16 11:02:51 2025 -0500
    
        Filter Rework
    
        Split different filter types into their own header files.
        Renamed BiQuad filter classes and grouped them in a namespace.
        Replaced the linked filter logic from the base filter class with a wrapper class that can create cascaded filters of any order.
        Added documentation for filter classes.
    
        Fixed comment inconsistency on namespace end-braces.

--- bdsp_dsp/Math/bdsp_MultiDimensionalLookup.h
+++ bdsp_dsp/Math/bdsp_MultiDimensionalLookup.h
@@ -1,17 +1,35 @@
 #pragma once
-
-
 namespace bdsp
 {
 	namespace dsp
 	{
+
+		/**
+		 * A Lookup table that stores the pre-calculated values that depend on 2 seperate variables.
+		 * Intermediate values are approximated using bilinear interpolation.
+		 * https://en.wikipedia.org/wiki/Bilinear_interpolation
+		 */
 		template <typename T>
 		class Lookup_2D
 		{
 		public:
 
+			/**
+			 * Creates and empty lookup table. This will be useless until initialized with a function.
+			 */
 			Lookup_2D() = default;
 
+
+			/**
+			 * Creates a lookup table that pre-calculates a given function of 2 variables
+			 * @param functionToApproximate The function to pre calculate
+			 * @param minInputValueToUseX The minimum input value expected for the first variable of the function
+			 * @param maxInputValueToUseX The maximum input value expected for the first variable of the function
+			 * @param minInputValueToUseY The minimum input value expected for the second variable of the function
+			 * @param maxInputValueToUseY The maximum input value expected for the second variable of the function
+			 * @param numPointsX The number of values to pre-calculate per value of the second variable
+			 * @param numPointsY The number of values to pre-calculate per value of the first variable
+			 */
 			Lookup_2D(const std::function<T(T, T)>& functionToApproximate,
 				T minInputValueToUseX,
 				T maxInputValueToUseX,
@@ -24,6 +42,16 @@ namespace bdsp
 			}
 
 
+			/**
+			 * Pre-calculates a given function of 2 variables
+			 * @param functionToApproximate The function to pre calculate
+			 * @param minInputValueToUseX The minimum input value expected for the first variable of the function
+			 * @param maxInputValueToUseX The maximum input value expected for the first variable of the function
+			 * @param minInputValueToUseY The minimum input value expected for the second variable of the function
+			 * @param maxInputValueToUseY The maximum input value expected for the second variable of the function
+			 * @param numPointsX The number of values to pre-calculate per value of the second variable
+			 * @param numPointsY The number of values to pre-calculate per value of the first variable
+			 */
 			void initialise(const std::function<T(T, T)>& functionToApproximate,
 				T minInputValueToUseX,
 				T maxInputValueToUseX,
@@ -56,75 +84,74 @@ namespace bdsp
 
 
 
-
+				//================================================================================================================================================================================================
+				// pre-calculate each point
 				for (size_t x = 0; x < dimX; x++)
 				{
 					for (size_t y = 0; y < dimY; y++)
 					{
-						auto unScaledX = (x - offsetX) / scalerX;
-						auto unScaledY = (y - offsetY) / scalerY;
+						auto value = functionToApproximate(convertXIndexToInput(x), convertYIndexToInput(y));
 
-
-						auto value = functionToApproximate(unScaledX, unScaledY);
-
+						/* Make sure functionToApproximate returns a sensible value for the entire specified range */
 						jassert(!std::isnan(value));
 						jassert(!std::isinf(value));
-						// Make sure functionToApproximate returns a sensible value for the entire specified range.
-						// E.g., this won't work for zero:  [] (size_t i) { return 1.0f / i; }
 
 						data.getReference(getIndex(x, y)) = value;
 
 					}
 				}
+				//================================================================================================================================================================================================
 				initialized = true;
 			}
 
-			//https://en.wikipedia.org/wiki/Bilinear_interpolation
+			/**
+			* Approximates a value from 2 given variable values.
+			* This will not check that these variables are in the expected range.
+			* If you are unsure if your inputs are in range call processSample instead.
+			*/
 			T processSampleUnchecked(T x, T y) const noexcept
 			{
 
+				T xIdx = convertXInputToIndex(x);
+				size_t x1 = floor(xIdx);
+				size_t x2 = ceil(xIdx);
 
-				auto scaledX = scalerX * x + offsetX;
-				auto xFloor = static_cast<size_t>(scaledX);
-				if (xFloor == dimX - 1)
+
+				T yIdx = convertYInputToIndex(y);
+				size_t y1 = floor(yIdx);
+				size_t y2 = ceil(yIdx);
+
+
+				if (x1 == x2 && y1 == y2) // trivial case where both inputs exactly match a pre-calculated value
 				{
-					xFloor--;
+					return getValue(x1, y1);
 				}
-
-				auto scaledY = scalerY * y + offsetY;
-				auto yFloor = static_cast<size_t>(scaledY);
-				if (yFloor == dimY - 1)
+				else if (x1 == x2) // case where only the x-input matches a pre-calculated x-input
 				{
-					yFloor--;
+					return juce::jmap(yIdx - y1, getValue(x1, y1), getValue(x1, y2));
 				}
+				else if (y1 == y2) // case where only the y-input matches a pre-calculated y-input
+				{
+					return juce::jmap(xIdx - x1, getValue(x1, y1), getValue(x2, y1));
+				}
+				else // desired value lies between a set of 4 pre-calculated values
+				{
+					T endpoint1 = juce::jmap(xIdx - x1, getValue(x1, y1), getValue(x2, y1));
+					T endpoint2 = juce::jmap(xIdx - x1, getValue(x1, y2), getValue(x2, y2));
 
-
-				T xDiff = scaledX - T(xFloor);
-
-
-				T yDiff = scaledY - T(yFloor);
-
-
-
-				T R1 = xDiff * data.getUnchecked(getIndex(xFloor, yFloor)) + (1 - xDiff) * data.getUnchecked(getIndex(xFloor + 1, yFloor));
-				T R2 = yDiff * data.getUnchecked(getIndex(xFloor, yFloor + 1)) + (1 - yDiff) * data.getUnchecked(getIndex(xFloor + 1, yFloor + 1));
-
-
-
-
-
-				return juce::jmap(yDiff, R1, R2);
-
+					return juce::jmap(yIdx - y1, endpoint1, endpoint2);
+				}
 			}
 
 
+			/**
+			 * Approximates a value from 2 given variable values.
+			 * This will limit these values to the expected range.
+			 */
 			T processSample(T x, T y) const noexcept
 			{
 				auto xLimited = juce::jlimit(minInputValueX, maxInputValueX, x);
-
-
 				auto yLimited = juce::jlimit(minInputValueY, maxInputValueY, y);
-
 
 				return processSampleUnchecked(xLimited, yLimited);
 			}
@@ -150,6 +177,28 @@ namespace bdsp
 				return x + y * dimX;
 			}
 
+			T getValue(size_t x, size_t y) const
+			{
+				return data[getIndex(x, y)];
+			}
+
+			T convertXInputToIndex(T xInput) const
+			{
+				return scalerX * xInput + offsetX;
+			}
+			T convertYInputToIndex(T yInput) const
+			{
+				return scalerY * yInput + offsetY;
+			}
+
+			T convertXIndexToInput(size_t xIndex) const
+			{
+				return (xIndex - offsetX) / scalerX;
+			}
+			T convertYIndexToInput(size_t yIndex) const
+			{
+				return (yIndex - offsetY) / scalerY;
+			}
 
 			//==============================================================================
 			juce::Array<T> data;
@@ -158,8 +207,10 @@ namespace bdsp
 
 			T minInputValueX, maxInputValueX;
 			T minInputValueY, maxInputValueY;
-			T scalerX, offsetX;
-			T scalerY, offsetY;
+
+			T scalerX, scalerY; // Multiplies the input to convert to an index in the data array
+			T offsetX, offsetY; // Multiplies the input to convert to an index in the data array
+
 			bool initialized = false;
 
 
