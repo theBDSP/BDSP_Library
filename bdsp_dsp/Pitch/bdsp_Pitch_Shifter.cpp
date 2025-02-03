b6d2a7580be8c502dd83aa90681cf8ed79d58392
Ben Shafran <bdsp@thebdsp.com>
2025-02-03 12:37:55 -0500

    Updated Pitch Shifter
    Added documentation to pitch shifter class.
    
    Removed stereo pitch shifter class and added multi-channel capabilities to the base pitch shifter class.

--- bdsp_dsp/Pitch/bdsp_Pitch_Shifter.cpp
+++ bdsp_dsp/Pitch/bdsp_Pitch_Shifter.cpp
@@ -9,14 +9,20 @@ namespace bdsp
 		{
 		}
 
-
-
 		template<typename SampleType>
 		void PitchShifter<SampleType>::prepare(const juce::dsp::ProcessSpec& spec)
 		{
 			BaseProcessingUnit<SampleType>::prepare(spec);
 
-			stretch.presetCheaper(spec.numChannels, spec.sampleRate);
+			pitchAmt.clear();
+			stretch.clear();
+			for (size_t c = 0; c < spec.numChannels; ++c)
+			{
+				stretch.add(new signalsmith::stretch::SignalsmithStretch<SampleType>());
+				stretch[c]->presetCheaper(1, spec.sampleRate);
+
+				pitchAmt.add(new juce::SmoothedValue<SampleType>());
+			}
 
 			latencyComp.setLatency(getLatency());
 			latencyComp.prepare(spec);
@@ -28,106 +34,50 @@ namespace bdsp
 		void PitchShifter<SampleType>::reset()
 		{
 			BaseProcessingUnit<SampleType>::reset();
-			stretch.reset();
+
+			for (auto* s : stretch)
+			{
+				s->reset();
+			}
 			latencyComp.reset();
 		}
 
 		template<typename SampleType>
-		void PitchShifter<SampleType>::initShiftAmount(SampleType semitones)
+		void PitchShifter<SampleType>::initShiftAmount(int channel, SampleType semitones)
 		{
-			pitchAmt.setCurrentAndTargetValue(semitones);
+			pitchAmt[channel]->setCurrentAndTargetValue(semitones);
 		}
 		template<typename SampleType>
-		void PitchShifter<SampleType>::setShiftAmount(SampleType semitones)
+		void PitchShifter<SampleType>::setShiftAmount(int channel, SampleType semitones)
 		{
-			pitchAmt.setTargetValue(semitones);
+			pitchAmt[channel]->setTargetValue(semitones);
 		}
 
 		template<typename SampleType>
 		void PitchShifter<SampleType>::setSmoothingTime(SampleType timeInSeconds)
 		{
 			BaseProcessingUnit<SampleType>::setSmoothingTime(timeInSeconds);
-			pitchAmt.reset(BaseProcessingUnit<SampleType>::sampleRate, timeInSeconds);
+			for (auto& p : pitchAmt)
+			{
+				p->reset(BaseProcessingUnit<SampleType>::sampleRate, timeInSeconds);
+			}
+
 		}
 
-
-
-		template<typename SampleType>
-		StereoSample<SampleType> PitchShifter<SampleType>::processSampleStereo(const StereoSample<SampleType>& inputSample) noexcept
-		{
-			return StereoSample<SampleType>();
-		}
 
 		template<typename SampleType>
 		void PitchShifter<SampleType>::updateSmoothedVariables()
 		{
 			BaseProcessingUnit<SampleType>::updateSmoothedVariables();
 
-			pitchAmt.getNextValue();
-		}
-
-		//================================================================================================================================================================================================
-
-			//================================================================================================================================================================================================
-
-
-		template<typename SampleType>
-		StereoPitchShifter<SampleType>::StereoPitchShifter()
-		{
-		}
-		template<typename SampleType>
-		void StereoPitchShifter<SampleType>::initShiftAmount(SampleType semitonesL, SampleType semitonesR)
-		{
-			pitchAmtL.setCurrentAndTargetValue(semitonesL);
-			pitchAmtR.setCurrentAndTargetValue(semitonesR);
-		}
-		template<typename SampleType>
-		void StereoPitchShifter<SampleType>::setShiftAmount(SampleType semitonesL, SampleType semitonesR)
-		{
-			pitchAmtL.setTargetValue(semitonesL);
-			pitchAmtR.setTargetValue(semitonesR);
-		}
-		template<typename SampleType>
-		void StereoPitchShifter<SampleType>::prepare(const juce::dsp::ProcessSpec& spec)
-		{
-			BaseProcessingUnit<SampleType>::prepare(spec);
-
-			stretchL.presetCheaper(1, spec.sampleRate);
-			stretchR.presetCheaper(1, spec.sampleRate);
-
-			latencyComp.setLatency(getLatency());
-			latencyComp.prepare(spec);
-			reset();
-		}
-		template<typename SampleType>
-		void StereoPitchShifter<SampleType>::reset()
-		{
-			BaseProcessingUnit<SampleType>::reset();
-			stretchL.reset();
-			stretchR.reset();
-			latencyComp.reset();
-		}
-
-		template<typename SampleType>
-		void StereoPitchShifter<SampleType>::updateSmoothedVariables()
-		{
-			BaseProcessingUnit<SampleType>::updateSmoothedVariables();
-
-			pitchAmtL.getNextValue();
-			pitchAmtR.getNextValue();
-		}
-		template<typename SampleType>
-		void StereoPitchShifter<SampleType>::setSmoothingTime(SampleType timeInSeconds)
-		{
-			BaseProcessingUnit<SampleType>::setSmoothingTime(timeInSeconds);
-			pitchAmtL.reset(BaseProcessingUnit<SampleType>::sampleRate, timeInSeconds);
-			pitchAmtR.reset(BaseProcessingUnit<SampleType>::sampleRate, timeInSeconds);
+			for (auto* p : pitchAmt)
+			{
+				p->getNextValue();
+			}
 		}
 
 		template class PitchShifter<float>;
 		template class PitchShifter<double>;
 
-		template class StereoPitchShifter<float>;
-		template class StereoPitchShifter<double>;
 	} // namespace dsp
 } // namespace bdsp
